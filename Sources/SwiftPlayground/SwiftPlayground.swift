edc4fa3aa0e46042387e7c836bc798e6002649ae
oc-elliot-rostain-25 <elliot.rostain@student.onslow.school.nz>
2026-02-04 23:29:39 +0000

    working on the task

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -5,5 +5,29 @@
 struct SwiftPlayground {
     static func main() {
         print("Hello, world!")
+
+
+
+print ("Enter the lengh of the room:")
+    if let userInputLength = readLine(), let roomLength = Double(userInputLength) {
+        print ("Enter the width of the room:  ")
+    if let userInputWidth = readLine(), let roomWidth = Double(userInputWidth) {
+                print ("Enter the height of the room:")
+    if let userInputHeight = readLine(), let roomHeight = Double(userInputHeight) {
+        let roomArea = roomLength * roomWidth
+        print ("The Area of the room is \(roomArea)m²")
+        print ("The height of the room is \(roomHeight)m")
+        let roomVolume = roomWidth * roomLength * roomHeight
+        print ("The volume ofthe room is \(roomVolume)m³")
+    }
+    }
+        }
+
+
+
+
+
+
+ 
     }
 }

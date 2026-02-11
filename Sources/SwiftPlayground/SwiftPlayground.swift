2dc7033452e089c9bcc2fd9126e87bdef251b7c4
oc-elliot-rostain-25 <elliot.rostain@student.onslow.school.nz>
2026-02-11 21:37:50 +0000

    finished task a, b, c, and d

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,9 +1,34 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+func printRoomIntro() {
+    print("This program calculates room area and volume.")
+    print("It also finds the usable space after furniture.")
+}
+
+func printDivider() {
+    print("--------")
+}
+
+func printUnitsNote() {
+    print("All measurements are in metres.")
+}
+
+func printGoodbye() {
+    print("Done. Thanks for using the calculator.")
+}
+
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        printRoomIntro()
+        
+        printDivider()
+        print("Summary here")
+        printDivider()
+
+        printUnitsNote()
+
+        printGoodbye()
     }
 }

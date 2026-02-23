739f4e0686b305e53f9af8144e0f9fc437ed4335
oc-elliot-rostain-25 <elliot.rostain@student.onslow.school.nz>
2026-02-23 21:08:43 +0000

    Finished task

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,8 +1,9 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+/// Creates a function that will print the egg shop menue.
 func eggMenue() {
-
+    print("")
     print("==== Egg Shop ====")
     print("1. Add eggs")
     print("2. Sell eggs")
@@ -11,70 +12,121 @@ func eggMenue() {
     print("5. Exit")
     print("Choose an option:")
 
-var currentEggStock = 0
+}
 
+//  Creates a variable that will store the egg stock.
+// Nonisolating makes so that the program doesnt crash because of shared mutable state
+nonisolated(unsafe)
+    var currentEggStock = 0
+
+// Creates a variable that will store the number of egg sold.
+// Nonisolating makes so that the program doesnt crash because of shared mutable state
+nonisolated(unsafe)
+    var totalEggsSold = 0
+
+/// Creates a function that caclulates the number of egg to add/
 func addEggs() {
-        let minimumEggs = 0
-        let maximumEggs = 1001
-        print("Do you want to add any eggs?")
-        if let input = readLine() {
-            if input == "Yes" {
-                print("How many Eggs do you want to add?")
-                if let eggInput = readLine(), let eggNumber = Int(eggInput),
-                    eggNumber > minimumEggs,
-                    eggNumber < maximumEggs
-                {
-                    print("Added \(eggNumber) eggs to stock")
-                    currentEggStock = currentEggStock + eggNumber
-                    return addEggs()
-                } else {
-                    print("Enter a valid number of eggs (between 1-1000)")
-                    return addEggs()
-                }
-            } else if input == "No" {
-                eggMenue()
-                userInput()
 
-            } else {
-                print("Please answer with Yes or No")
-                return addEggs()
-            }
-        }
+// Sets the minimum number of eggs you can add
+    let minimumEggs = 0
 
+// Sets the maximum number of eggs you can add.
+    let maximumEggs = 1001
 
-    func userInput() {
+// Asks the user for input and adds requirments.
+    print("How many Eggs do you want to add?")
+    if let eggInput = readLine(), let eggNumber = Int(eggInput), eggNumber > minimumEggs,
+        eggNumber < maximumEggs
+    {
+        // Adds the user input number to the egg Stock.
+        print("Added \(eggNumber) eggs to stock")
+        currentEggStock = currentEggStock + eggNumber
 
-        let minimum = 0
-        let maximum = 6
+    } else {
 
-        if let input = readLine(), let number = Int(input), number > minimum,
-            number < maximum
-        {
-            if number == 1 {
-                addEggs()
-            } else if number == 3 {
-                print ("Current egg stock is of \(currentEggStock) eggs")
-            }
-        } else {
-            print("Please choose a valid option.")
-            return userInput()
-        }
+        // If the input doesnt meet the requirments, restarts the function.
+        print("Enter a valid number of eggs (between 1-1000)")
+        return addEggs()
     }
 
+}
 
-        @main
-        struct SwiftPlayground {
-            static func main() {
+/// Creates a function that will calculate and store the number of eggs to be sold.
+func sellEggs() {
+    let minimumSold = 0
 
-                var menuRunning = true
 
-                while menuRunning == true {
-                    eggMenue()
-                    userInput()
-                    menuRunning = false
+// Asks the user for input to sell eggs and sets requirments.
+    print("Enter how many eggs you want to sell:")
+    if let sellInput = readLine(), let sellNumber = Int(sellInput), sellNumber > minimumSold,
+        sellNumber < currentEggStock
+    {
+
+        // Substrack the number of eggs from the egg stock.
+        // Adds the number of eggs to the total eggs sold.
+        print("Sold \(sellNumber) eggs")
+        currentEggStock = currentEggStock - sellNumber
+        totalEggsSold = totalEggsSold + sellNumber
+
+    } else {
+
+        // If the input doesnt meet the requirments, sends a message and restarts the function.
+        print("Enter a valid number of eggs (under the total in stock)")
+        return sellEggs()
+    }
+}
+
+@main
+struct SwiftPlayground {
+    static func main() {
+
+// Sets the minimum number the user can input for the main menu
+        let minimum = 0
+
+// Sets the maximum number the user can input for the main menu
+        let maximum = 6
+
+// Creates a variable that will control when the while loop is active.
+        var isRunning = true
+
+// Creates a while loop to keep showing the menu until the user exits it.
+        while isRunning == true {
+
+            // Runs the eggMenu function
+            eggMenue()
+
+// Ask the user for input so it can send him to the rigth place he cose on the menu.
+            if let input = readLine(), let option = Int(input), option < maximum, option > minimum {
+
+// If the input is 1, runs the add Eggs function
+                if option == 1 {
+                    addEggs()
                 }
-            }
 
+// If the Input is 2, runs the sell eggs fuction
+                if option == 2 {
+                    sellEggs()
+                }
+
+// If the input is 3, shows the user the Egg Stock
+                if option == 3 {
+                    print("Current egg stock : \(currentEggStock) eggs")
+                }
+
+                // If the input is 4, shows the user the total egg sold.
+                if option == 4 {
+                    print(" Total eggs sold :\(totalEggsSold) eggs")
+                }
+
+// If the input is 5, exits the program
+                if option == 5 {
+                    isRunning = false
+                }
+
+                // If the input doesnt meet the requirments, restarts the program and shows an error message.
+            } else {
+                print("Enter a valid number")
+            }
         }
     }
 }

ca74c461be00989b3ea0b9f89dfc474f6297cfff
oc-elliot-rostain-25 <elliot.rostain@student.onslow.school.nz>
2026-03-13 02:07:58 +0000

    working on code

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,34 +1,72 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-
 func printBoard(_ board: [[String]]) {
 
-}
-
-func processGuess(row: Int, col: Int, ocean: [[String]], guesses: [[String]]) -> [[String]] {
-    
 }
 
 @main
 struct SwiftPlayground {
     static func main() {
 
-        let size = 6
-        var ocean = Array(repeating: Array(repeating: "~", count: size), count: size)
-        var guesses = Array(repeating: Array(repeating: "~", count: size), count: size)
+        var player = "S"
+        var ocean = [
+            ["~", "~", "~", "~", "~", "~"],
+            ["~", "~", "~", "~", "~", "~"],
+            ["~", "~", "~", "~", "~", "~"],
+            ["~", "~", "~", "~", "~", "~"],
+            ["~", "~", "~", "~", "~", "~"],
+            ["~", "~", "~", "~", "~", "~"],
+        ]
 
-        ocean[1][3] = "S"
-ocean[2][3] = "S"
-ocean[4][0] = "S"
-ocean[5][4] = "S"
-print("Board: ")
-print(ocean)
-print("")
-print("--------")
-print("")
-print("Guesses:")
-print(guesses)
+        var answers = [
+            ["~", "~", "S", "~", "~", "~"],
+            ["~", "S", "~", "~", "~", "S"],
+            ["S", "~", "~", "~", "~", "S"],
+            ["~", "~", "~", "~", "~", "~"],
+            ["~", "S", "S", "S", "S", "~"],
+            ["~", "~", "~", "~", "~", "~"],
+        ]
+        print(board: ocean)
 
+        while true {
+            let position = positionInput(board: ocean)
+            ocean[position[0]][position[1]] = player
+
+            if player == "S" {
+                player = "O"
+            } else {
+                player = "O"
+            }
+
+            print(board: ocean)
+        }
+
+    }
+}
+
+func print(board: [[String]]) {
+    board.forEach { line in
+        print("\(line[0]) | \(line[1]) | \(line[2]) | \(line[3]) | \(line[4]) | \(line[5])")
+        print("----------------------")
+    }
+    print("")
+}
+
+func positionInput(board: [[String]]) -> [Int] {
+    while true {
+        print("Enter a row number 1-6:")
+        let userInput = readLine()!
+        let rowNumber = Int(userInput)! - 1
+
+        print("Enter a column number 1-6:")
+        let userInput2 = readLine()!
+        let columnNumber = Int(userInput2)! - 1
+
+        if board[rowNumber][columnNumber] == "~" {
+            return [rowNumber, columnNumber]
+        } else if board[rowNumber][columnNumber] == "" {
+            return [rowNumber, columnNumber]
+        }
     }
 }

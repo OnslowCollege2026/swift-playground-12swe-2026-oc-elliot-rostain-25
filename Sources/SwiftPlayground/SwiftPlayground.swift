f4049e4b5e7ffe48a41d20ddee0bff9c6ec58b44
oc-elliot-rostain-25 <elliot.rostain@student.onslow.school.nz>
2026-03-16 23:44:17 +0000

    Working on task almost finished

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,15 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-func printBoard(_ board: [[String]]) {
-
-}
-
 @main
 struct SwiftPlayground {
     static func main() {
 
-        var player = "S"
         var ocean = [
             ["~", "~", "~", "~", "~", "~"],
             ["~", "~", "~", "~", "~", "~"],
@@ -27,23 +22,35 @@ struct SwiftPlayground {
             ["~", "S", "S", "S", "S", "~"],
             ["~", "~", "~", "~", "~", "~"],
         ]
+
+        
+let fromRow: [Int] = [0, 1, 0, 5, 5, 5, 5, 1, 2]
+let fromCol: [Int] = [2, 1, 2, 1, 2, 3, 4, 6, 6]
+
+// Generate new random position
+let toRow: Int = Int.random(in: 0..<answers.count)
+let toCol: Int = Int.random(in: 0..<answers[0].count)
+
+//Swap the letters
+let randoShip: Int = answers[fromRow][fromCol]
+answers[fromRow][fromCol] = answers[toRow][toCol]
+answers[toRow][toCol] = randoShip
+
         print(board: ocean)
 
         while true {
-            let position = positionInput(board: ocean)
+            let position = positionInput(board: ocean, board2: answers)
             ocean[position[0]][position[1]] = player
-
-            if player == "S" {
-                player = "O"
-            } else {
-                player = "O"
-            }
 
             print(board: ocean)
         }
 
     }
 }
+
+
+nonisolated(unsafe)
+    var player = "O"
 
 func print(board: [[String]]) {
     board.forEach { line in
@@ -53,19 +60,21 @@ func print(board: [[String]]) {
     print("")
 }
 
-func positionInput(board: [[String]]) -> [Int] {
+func positionInput(board: [[String]], board2: [[String]]) -> [Int] {
     while true {
         print("Enter a row number 1-6:")
         let userInput = readLine()!
         let rowNumber = Int(userInput)! - 1
 
         print("Enter a column number 1-6:")
-        let userInput2 = readLine()!
-        let columnNumber = Int(userInput2)! - 1
+        let guess = readLine()!
+        let columnNumber = Int(guess)! - 1
 
-        if board[rowNumber][columnNumber] == "~" {
+        if board2[rowNumber][columnNumber] == "~" {
+            player = "O"
             return [rowNumber, columnNumber]
-        } else if board[rowNumber][columnNumber] == "" {
+        } else if board2[rowNumber][columnNumber] == "S" {
+            player = "S"
             return [rowNumber, columnNumber]
         }
     }

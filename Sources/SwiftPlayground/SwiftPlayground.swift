c13d0e77bdd17f541a38d5a31666098cbfaca310
oc-elliot-rostain-25 <elliot.rostain@student.onslow.school.nz>
2026-05-15 08:12:28 +0000

    ~finishd the code

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,140 +1,212 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-/// Creates a function that will print the egg shop menue.
-func eggMenue() {
-    print("")
-    print("==== Egg Shop ====")
-    print("1. Add eggs")
-    print("2. Sell eggs")
-    print("3. Show current stock")
-    print("4. Show total eggs sold")
-    print("5. Reset egg shop")
-    print("6. Exit")
-    print("Choose an option:")
-
-}
-
-//  Creates a variable that will store the egg stock.
-// Nonisolating makes so that the program doesnt crash because of shared mutable state
-nonisolated(unsafe)
-    var currentEggStock = 0
-
-// Creates a variable that will store the number of egg sold.
-// Nonisolating makes so that the program doesnt crash because of shared mutable state
-nonisolated(unsafe)
-    var totalEggsSold = 0
-
-/// Creates a function that caclulates the number of egg to add/
-func addEggs() {
-
-// Sets the minimum number of eggs you can add
-    let minimumEggs = 0
-
-// Sets the maximum number of eggs you can add.
-    let maximumEggs = 1001
-
-// Asks the user for input and adds requirments.
-    print("How many Eggs do you want to add?")
-    if let eggInput = readLine(), let eggNumber = Int(eggInput), eggNumber > minimumEggs,
-        eggNumber < maximumEggs
-    {
-        // Adds the user input number to the egg Stock.
-        print("Added \(eggNumber) eggs to stock")
-        currentEggStock = currentEggStock + eggNumber
-
-    } else {
-
-        // If the input doesnt meet the requirments, restarts the function.
-        print("Enter a valid number of eggs (between 1-1000)")
-        return addEggs()
-    }
-
-}
-
-/// Creates a function that will calculate and store the number of eggs to be sold.
-func sellEggs() {
-    let minimumSold = 0
-
-
-// Asks the user for input to sell eggs and sets requirments.
-    print("Enter how many eggs you want to sell:")
-    if let sellInput = readLine(), let sellNumber = Int(sellInput), sellNumber > minimumSold,
-        sellNumber < currentEggStock
-    {
-
-        // Substrack the number of eggs from the egg stock.
-        // Adds the number of eggs to the total eggs sold.
-        print("Sold \(sellNumber) eggs")
-        currentEggStock = currentEggStock - sellNumber
-        totalEggsSold = totalEggsSold + sellNumber
-
-    } else {
-
-        // If the input doesnt meet the requirments, sends a message and restarts the function.
-        print("Enter a valid number of eggs (under the total in stock)")
-        return sellEggs()
-    }
-}
-
-@main
-struct SwiftPlayground {
+@main struct SwiftPlayground {
     static func main() {
 
-// Sets the minimum number the user can input for the main menu
-        let minimum = 0
+        // Current amount of kumara in the bin
+        var currentStock = 0.0
 
-// Sets the maximum number the user can input for the main menu
-        let maximum = 7
+        // Max kg the bin can hold
+        let maxStock = 50.0
 
-// Creates a variable that will control when the while loop is active.
-        var isRunning = true
+        // Price per kg of kumara
+        let kgPrice = 3.00
 
-// Creates a while loop to keep showing the menu until the user exits it.
-        while isRunning == true {
+        // Price per bag
+        let bagPrice = 0.20
 
-            // Runs the eggMenu function
-            eggMenue()
+        // Mmax weight in a bag
+        let maxBagWeight = 5.0
 
-// Ask the user for input so it can send him to the rigth place he cose on the menu.
-            if let input = readLine(), let option = Int(input), option < maximum, option > minimum {
+        // minimum weight to buy
+        let minWeight = 0.1
 
-// If the input is 1, runs the add Eggs function
-                if option == 1 {
-                    addEggs()
+        // number of sales (adding this everytime to the total of sales)
+        let addSale = 1
+
+        // Helps round up the number of bags i need
+        let helpsMeRound = 0.999
+
+        // Used to check if there are no sales yet
+        let noSale = 0
+
+        // THe 2d array: weight sold, bags used, total price/charge
+        var sales = [[Double]]()
+
+        let minimum = 0.0
+
+        var running = true
+
+        while running {
+
+            // Main menu for the Stall/Shop
+            print("")
+            print("=== Kumara Shop === ")
+            print("1. Add stock")
+            print("2. Record a sale")
+            print("3. See stock")
+            print("4. See sales")
+            print("5. See summary")
+            print("0. Exit")
+            print("Enter a number:")
+
+            // Lets the user enter the number of their choice
+            let choice = readLine()
+
+            // if the choice is 0, exits the program
+            if choice == "0" {
+
+                print("Goodbyeeee")
+                running = false
+
+                // if the choice is 1, asks the user to add stock to the kumara bin
+            } else if choice == "1" {
+
+                // Ask the user how much stock to add to  the bin
+                print("Current stock is \(currentStock) kg")
+                print("How many kg do you wanna add?")
+
+                // records the userInput to know how much they want to add
+                let input = readLine()
+
+                if let amountText = input, let amount = Double(amountText) {
+                    if amount <= minimum {
+
+                        // if the amount is under 0 or qual, then it doesnt accept
+                        print("enter a valid number")
+                    } else if currentStock + amount > maxStock {
+
+                        // if the amount is over the maximum, it doesnt accept either
+                        print("Cannot put more than 50 kg in the bin")
+                    } else {
+
+                        // updtaes the stock if the amount if valid
+                        currentStock = currentStock + amount
+                        print("Stock is now \(currentStock) kg")
+                    }
+                } else {
+                    print("enter a valid NUMBER")
                 }
 
-// If the Input is 2, runs the sell eggs fuction
-                if option == 2 {
-                    sellEggs()
+            } else if choice == "2" {
+
+                // Get the weight sold, calculate bags and charges, then save the sale
+                if currentStock <= minimum {
+                    print("No stock left :(")
+                } else {
+
+                    // informs the user of the current stock (maximum to be entered)
+                    print("Current Stock: \(currentStock) kg")
+                    print("Enter weight sold (kg):")
+
+                    // asks for the user input to enter a weight to sell
+                    let userInput = readLine()
+
+                    if let weightString = userInput, let weight = Double(weightString) {
+
+                        if weight < minWeight {
+                            print("Minimum weight is \(minWeight) kg.")
+                        } else if weight > currentStock {
+                            print("Sadly, there is not enough stock...")
+                        } else {
+
+                            // Automatically calculates many bags are needed
+                            // theo helpsMeRound is used to round up as Int rounds down
+                            // meaning that it could give a wrong number of bags
+                            // Only solution I found
+                            let bags = Int((weight / maxBagWeight) + helpsMeRound)
+
+                            // calaculates the price of the weight of kumara the user wants
+                            let kumaraCharge = (weight * kgPrice)
+
+                            // calaculates the price of the bag(s)
+                            let bagCharge = Double(bags) * bagPrice
+
+                            // adds up the 2 charges to make the total charge
+                            let totalCharge = kumaraCharge + bagCharge
+
+                            // prints a small summary of the purchase
+                            print("")
+                            print("Bags needed:   \(bags)")
+                            print("Kumara price: $\(kumaraCharge)")
+                            print("Bag price:    $\(bagCharge)")
+                            print("Total charge:  $\(totalCharge)")
+
+                            // Update stock and save the sale to the 2D array
+                            currentStock = currentStock - weight
+                            sales.append([weight, Double(bags), totalCharge])
+                            print("Stock changed to \(currentStock) kg")
+                        }
+
+                    } else {
+                        // if not a valid number, then tells the user
+                        print("That was not a valid number :/")
+                    }
                 }
 
-// If the input is 3, shows the user the Egg Stock
-                if option == 3 {
-                    print("Current egg stock : \(currentEggStock) eggs")
+            } else if choice == "3" {
+
+                // shows current stock to the user.
+                print("Current stock: \(currentStock) kg")
+
+            } else if choice == "4" {
+
+                // Looks in the 2D array and print every sale
+                if sales.count == noSale {
+                    print("No sales yet")
+                } else {
+                    for saleNumber in 0..<sales.count {
+
+                        // creates constants from the 2d array to use right after
+                        let weight = sales[saleNumber][0]
+                        let bags = Int(sales[saleNumber][1])
+                        let total = sales[saleNumber][2]
+                        print(
+                            "Sale \(saleNumber + addSale): \(weight) kg, \(bags) bag(s), $\(total)")
+                    }
                 }
 
-                // If the input is 4, shows the user the total egg sold.
-                if option == 4 {
-                    print(" Total eggs sold :\(totalEggsSold) eggs")
+            } else if choice == "5" {
+
+                // Add up all sales from the 2D array and show totals and averages
+                if sales.count == noSale {
+                    print("No sales yet")
+                } else {
+
+                    // total weight sold
+                    var totalWeight = 0.0
+
+                    // total bags used
+                    var totalBags = 0.0
+
+                    // total money earned
+                    var totalEarned = 0.0
+
+                    // For the summary, takes the values from the 2d array
+                    for saleNumber in 0..<sales.count {
+                        totalWeight = totalWeight + sales[saleNumber][0]
+                        totalBags = totalBags + sales[saleNumber][1]
+                        totalEarned = totalEarned + sales[saleNumber][2]
+                    }
+                    // claculates the average weight sold
+                    let averageWeight = totalWeight / totalBags
+
+                    // calculates the average money spent by buyers
+                    let averageMoney = totalEarned / totalBags
+
+                    // prints the total summary
+                    print(" ===== Summary ===== ")
+                    print("Total sales:       \(sales.count)")
+                    print("Total weight sold: \(totalWeight) kg")
+                    print("Total bags used:   \(totalBags)")
+                    print("Total earned:      $\(totalEarned)")
+                    print("Average kg per bag:    \(averageWeight) kg")
+                    print("Average $ per bag:     $\(averageMoney)")
                 }
 
-// If the input is 5, resets the values
-                if option == 5 {
-                    currentEggStock = 0
-                    totalEggsSold = 0
-                    print("Egg shop has been reset")
-                }
-
-// If the input is 6, exits the program
-                if option == 6 {
-                    print ("Thank you for using our egg tracker !!!")
-                    isRunning = false
-                }
-
-                // If the input doesnt meet the requirments, restarts the program and shows an error message.
             } else {
-                print("Enter a valid number")
+                print("Invalid number, try again buddy")
             }
         }
     }
